d88e2d736738f49051dac3044e67cffdd36a46f7
Anena V Sims <anenavsims@Anenas-MacBook-Air.local>
2016-10-09 19:04:08 -0500

    changed robot parts updated make view main and class diagram

---
+++ create_parts.cpp
@@ -0,0 +1,9 @@
+//
+//  create_parts.cpp
+//  
+//
+//  Created by Anena V Sims on 10/9/16.
+//
+//
+
+#include "create_parts.hpp"
---
+++ create_parts.hpp
@@ -0,0 +1,14 @@
+//
+//  create_parts.hpp
+//  
+//
+//  Created by Anena V Sims on 10/9/16.
+//
+//
+
+#ifndef create_parts_hpp
+#define create_parts_hpp
+
+#include <stdio.h>
+
+#endif /* create_parts_hpp */
